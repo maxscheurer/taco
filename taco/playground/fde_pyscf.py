75d14e055053133019f61d8a4157666d409c103d
Cristina E. González-Espinoza <crisbeth46@gmail.com>
2019-06-04 15:39:48 +0200

    Added rigth test geometries for CO and H2O

--- taco/playground/fde_pyscf.py
+++ taco/playground/fde_pyscf.py
@@ -1,14 +1,23 @@
 from pyscf import gto, scf
 
 # Run SCF in pyscf
-mol = gto.M(
-    atom='O 0 0 0;'
-         'H 0 0 1.795239827225189;'
-         'H 1.693194615993441 0 -0.599043184453037',
+h2o = gto.M(
+    atom="""
+            O  -7.9563726699    1.4854060709    0.1167920007
+            H  -6.9923165534    1.4211335985    0.1774706091
+            H  -8.1058463545    2.4422204631    0.1115993752
+         """,
     basis='sto-3g',
-    unit="Bohr"
 )
-scfres = scf.RHF(mol)
-scfres.conv_tol = 1e-10
-scfres.conv_tol_grad = 1e-6
+co = gto.M(
+    atom="""
+            C  -3.6180905689    1.3768035675   -0.0207958979
+            O  -4.7356838533    1.5255563000    0.1150239130
+         """,
+    basis='sto-3g',
+        )
+
+scfres = scf.RHF(h2o)
 scfres.kernel()
+scfres2 = scf.RHF(co)
+scfres2.kernel()
