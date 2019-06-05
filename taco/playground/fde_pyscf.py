ab2297700fae4c7301c08a908a13d26a795156c7
Maximilian Scheurer <max.scheurer@me.com>
2019-06-05 08:48:52 +0200

    facilitated supersystem molecule build

--- taco/playground/fde_pyscf.py
+++ taco/playground/fde_pyscf.py
@@ -21,16 +21,7 @@ co = gto.M(
          """,
     basis='sto-3g',
         )
-system = gto.M(
-    atom="""
-            C  -3.6180905689    1.3768035675   -0.0207958979
-            O  -4.7356838533    1.5255563000    0.1150239130
-            O  -7.9563726699    1.4854060709    0.1167920007
-            H  -6.9923165534    1.4211335985    0.1774706091
-            H  -8.1058463545    2.4422204631    0.1115993752
-         """,
-    basis='sto-3g',
-        )
+system = gto.M(atom=co.atom + h2o.atom)
 # Get initial densities from HF
 # H2O
 # TODO: make a wrapper and make sure DMs are correct
