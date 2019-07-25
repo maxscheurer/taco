980eb6a9092d4b0938525c33eb691caaf30446ba
Cristina E. González-Espinoza <crisbeth46@gmail.com>
2019-07-25 18:30:45 +0200

    Add psi4 the test to tests folder.

--- taco/tests/test_playground.py
+++ taco/tests/test_playground.py
@@ -2,7 +2,11 @@
 Test playground code for pyscf and psi4
 """
 from taco.playground.fde_pyscf import run_co_h2o_pyscf
+from taco.playground.fde_psi4 import run_co_h2o_psi4
 
 
 def test_playground_pyscf():
     run_co_h2o_pyscf()
+
+def test_playground_psi4():
+    run_co_h2o_psi4()
