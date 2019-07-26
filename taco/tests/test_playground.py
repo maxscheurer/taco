a602c8ba8b25de4cf57fcaa00714bebe90f9964a
Cristina E. González-Espinoza <crisbeth46@gmail.com>
2019-07-26 17:21:08 +0200

    Add calls to test_playground.

--- taco/tests/test_playground.py
+++ taco/tests/test_playground.py
@@ -1,13 +1,17 @@
 """
 Test playground code for pyscf and psi4
 """
-from taco.playground.fde_pyscf import run_co_h2o_pyscf
+from taco.playground.fde_pyscf import run_co_h2o_pyscf_sto3g, run_co_h2o_pyscf_dz
+from taco.playground.fde_pyscf import run_co_h2o_pyscf_tz, run_co_h2o_pyscf_qz
 from taco.playground.fde_psi4 import run_co_h2o_psi4_sto3g, run_co_h2o_psi4_dz
 from taco.playground.fde_psi4 import run_co_h2o_psi4_tz, run_co_h2o_psi4_qz
 
 
 def test_playground_pyscf():
-    run_co_h2o_pyscf()
+    run_co_h2o_pyscf_sto3g()
+    run_co_h2o_pyscf_dz()
+    run_co_h2o_pyscf_tz()
+    run_co_h2o_pyscf_qz()
 
 def test_playground_psi4():
     run_co_h2o_psi4_sto3g()
